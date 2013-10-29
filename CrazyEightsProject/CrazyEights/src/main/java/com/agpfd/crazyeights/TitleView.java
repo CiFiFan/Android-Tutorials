4603165203dd22035ae9a607ec90d33b41e3458e
Sendo Iajuvo <pacogames@hotmail.com>
2013-10-29 20:29:23 +0100

    Capitulo 3, añadido GameView

--- CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/TitleView.java
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/TitleView.java
@@ -3,6 +3,7 @@ package com.agpfd.crazyeights;
  * Created by sendo on 29/10/13.
  */
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -14,22 +15,22 @@ public class TitleView extends View {
 
     private Bitmap titleGraphic;
     private Bitmap playButtonUp;
+    private Bitmap playButtonDown;
     private int screenW;
     private int screenH;
+    private boolean playButtonPressed;
+    private Context myContext;
 
     public TitleView(Context context) {
         super(context);
-        titleGraphic =
-                BitmapFactory.decodeResource(getResources(),
-                        R.drawable.title_graphic);
-        playButtonUp =
-                BitmapFactory.decodeResource(getResources(),
-                        R.drawable.play_button_up);
+        myContext = context;
+        titleGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.title_graphic);
+        playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
+        playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
     }
 
     @Override
-    public  void  onSizeChanged (int w, int h, int oldw,
-                                 int oldh) {
+    public  void  onSizeChanged (int w, int h, int oldw, int oldh){
         super.onSizeChanged(w, h, oldw, oldh);
         screenW = w;
         screenH = h;
@@ -37,14 +38,14 @@ public class TitleView extends View {
 
     @Override
     protected  void  onDraw(Canvas canvas) {
-        canvas.drawBitmap(titleGraphic,
-                (screenW=titleGraphic.getWidth())/2, 0, null);
-        canvas.drawBitmap(playButtonUp,
-                (int)(screenW*0.5),
-                (int)(screenH*0.7), null);
+        canvas.drawBitmap(titleGraphic, (screenW-titleGraphic.getWidth())/2, 0, null);
+        if (playButtonPressed) {
+            canvas.drawBitmap(playButtonDown, (screenW-playButtonUp.getWidth())/2, (int)(screenH*0.7), null);
+        } else {
+            canvas.drawBitmap(playButtonUp, (screenW-playButtonUp.getWidth())/2, (int)(screenH*0.7), null);
+        }
     }
 
-    @Override
     public boolean onTouchEvent(MotionEvent event) {
         int eventaction = event.getAction();
         int X = (int)event.getX();
@@ -52,12 +53,23 @@ public class TitleView extends View {
 
         switch (eventaction) {
             case MotionEvent.ACTION_DOWN:
+                if (X > (screenW-playButtonUp.getWidth())/2 &&
+                        X < ((screenW-playButtonUp.getWidth())/2) + playButtonUp.getWidth() &&
+                        Y > (int)(screenH*0.7) &&
+                        Y < (int)(screenH*0.7) + playButtonUp.getHeight()) {
+                        playButtonPressed = true;
+                }
                 break;
 
             case MotionEvent.ACTION_MOVE:
                 break;
 
             case MotionEvent.ACTION_UP:
+                if (playButtonPressed) {
+                    Intent gameIntent = new Intent(myContext, GameActivity.class);
+                    myContext.startActivity(gameIntent);
+                }
+                playButtonPressed = false;
                 break;
         }
         invalidate();
