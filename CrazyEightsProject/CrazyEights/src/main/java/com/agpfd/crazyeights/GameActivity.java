566f1fdd5c56f337791f4cd9866dd366f1075e75
Sendo Iajuvo <pacogames@hotmail.com>
2013-10-29 20:33:49 +0100

    Capitulo 3, añadido GameView

--- CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameActivity.java
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameActivity.java
@@ -19,4 +19,5 @@ public class GameActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(gView);
     }
+
 }
