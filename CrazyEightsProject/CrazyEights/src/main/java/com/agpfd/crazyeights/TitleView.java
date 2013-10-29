f62a74bfeeaa168552bec56bb4d096c7fdd4f2a8
Sendo Iajuvo <pacogames@hotmail.com>
2013-10-29 12:16:57 +0100

    Primera subida del proyecto vacio creado automaticamente por android studio para Crazy Eights. Hay errores pero quiero probar conexion con Github

---
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/TitleView.java
@@ -0,0 +1,60 @@
+package com.agpfd.crazyeights;
+/**
+ * Created by sendo on 29/10/13.
+ */
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+
+public class TitleView extends View {
+
+    private Bitmap titleGraphic;
+    private int screenW;
+    private int screenH;
+
+    public TitleView(Context context) {
+        super(context);
+        titleGraphic =
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.title_graphic);
+    }
+
+    @Override
+    public  void  onSizeChanged (int w, int h, int oldw,
+                                 int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        screenW = w;
+        screenH = h;
+    }
+
+    @Override
+    protected  void  onDraw(Canvas canvas) {
+        canvas.drawBitmap(titleGraphic,
+                (screenW=titleGraphic.getWidth())/2, 0, null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventaction = event.getAction();
+        int X = (int)event.getX();
+        int Y = (int)event.getY();
+
+        switch (eventaction) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        invalidate();
+        return true;
+    }
+}
