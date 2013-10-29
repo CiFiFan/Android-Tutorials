1dd18b588f46310f5928ccbe763a479737b75249
Sendo Iajuvo <pacogames@hotmail.com>
2013-10-29 23:41:37 +0100

    Capitulo 6, repartiendo cartas

--- CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameView.java
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameView.java
@@ -1,17 +1,45 @@
 package com.agpfd.crazyeights;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by sendo on 29/10/13.
  */
 public class GameView extends View {
 
+    private int screenW;
+    private int screenH;
+    private Context myContext;
+    private List<Card> deck = new ArrayList<Card>();
+    private int scaledCardW;
+    private int scaledCardH;
+    private List<Card> myHand = new ArrayList<Card>();
+    private List<Card> oppHand = new ArrayList<Card>();
+    private List<Card> discardPile = new ArrayList<Card>();
+
     public GameView (Context context) {
         super(context);
+        myContext = context;
+    }
+
+    @Override
+    public void onSizeChanged (int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        screenW = w;
+        screenH = h;
+
+        initCards();
+        dealCards();
     }
 
     @Override
@@ -38,4 +66,41 @@ public class GameView extends View {
         invalidate();
         return true;
     }
+
+    private void initCards() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 102; j < 115; j++) {
+                int tempId = j + (i*100);
+                Card tempCard = new Card(tempId);
+                int resourceId = getResources().getIdentifier("card"+ tempId, "drawable", myContext.getPackageName());
+                Bitmap tempBitmap = BitmapFactory.decodeResource(myContext.getResources(), resourceId);
+                scaledCardW = (int) (screenW/8);
+                scaledCardH = (int) (scaledCardW*1.28);
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(tempBitmap, scaledCardW, scaledCardH, false);
+                tempCard.setBitmap(scaledBitmap);
+                deck.add(tempCard);
+            }
+        }
+    }
+
+    private void drawCard(List<Card> handToDraw) {
+        handToDraw.add(0, deck.get(0));
+        deck.remove(0);
+        if (deck.isEmpty()) {
+            for (int i = discardPile.size()-1; i > 0; i--) {
+                deck.add(discardPile.get(i));
+                discardPile.remove(i);
+                Collections.shuffle(deck, new Random());
+            }
+        }
+    }
+
+    private void dealCards() {
+        Collections.shuffle(deck, new Random());
+        for (int i = 0; i < 7; i++) {
+            drawCard(myHand);
+            drawCard(oppHand);
+        }
+    }
+
 }
