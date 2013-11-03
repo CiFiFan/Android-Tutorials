46b0954adc7b890995852706f15deb79e6902fec
Sendo Iajuvo <pacogames@hotmail.com>
2013-11-03 18:37:10 +0100

    Capitulo 6, repartiendo cartas

--- CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/Card.java
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/Card.java
@@ -8,10 +8,14 @@ import android.graphics.Bitmap;
 public class Card {
 
     private int id;
+    private int suit;
+    private int rank;
     private Bitmap bmp;
 
     public Card(int newId) {
         id = newId;
+        suit = Math.round((id/100)*100);
+        rank = id-suit;
     }
 
     public void setBitmap(Bitmap newBitmap) {
@@ -24,5 +28,13 @@ public class Card {
 
     public int getId() {
         return id;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
     }
 }
--- CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameView.java
+++ CrazyEightsProject/CrazyEights/src/main/java/com/agpfd/crazyeights/GameView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -26,10 +28,28 @@ public class GameView extends View {
     private List<Card> myHand = new ArrayList<Card>();
     private List<Card> oppHand = new ArrayList<Card>();
     private List<Card> discardPile = new ArrayList<Card>();
+    private float scale;
+    private Paint whitePaint;
+    private int oppScore;
+    private int myScore;
+    private Bitmap cardBack;
+    private boolean myTurn;
+    private int movingCardIdx = -1;
+    private int movingX;
+    private int movingY;
 
     public GameView (Context context) {
         super(context);
         myContext = context;
+        scale = myContext.getResources().getDisplayMetrics().density;
+        //myTurn = new Random().nextBoolean();
+        myTurn = true;
+        whitePaint = new Paint();
+        whitePaint.setAntiAlias(true);
+        whitePaint.setColor(Color.WHITE);
+        whitePaint.setStyle(Paint.Style.STROKE);
+        whitePaint.setTextAlign(Paint.Align.LEFT);
+        whitePaint.setTextSize(scale*15);
     }
 
     @Override
@@ -37,14 +57,39 @@ public class GameView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         screenW = w;
         screenH = h;
+        Bitmap tempBitmap = BitmapFactory.decodeResource(myContext.getResources(), R.drawable.card_back);
+        scaledCardW = (int) (screenW/8);
+        scaledCardH = (int) (scaledCardW*1.28);
+        cardBack = Bitmap.createScaledBitmap(tempBitmap, scaledCardW, scaledCardH, false);
 
         initCards();
         dealCards();
+        drawCard(discardPile);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        canvas.drawText("Computer Score: " + Integer.toString(oppScore), 10, whitePaint.getTextSize()+10, whitePaint);
+        canvas.drawText("My Score: " + Integer.toString(myScore), 10, screenH-whitePaint.getTextSize()-10, whitePaint);
 
+        for (int i = 0; i < oppHand.size(); i++) {
+            canvas.drawBitmap(cardBack, i*(scale*5), whitePaint.getTextSize()+(50*scale), null);
+        }
+
+        canvas.drawBitmap(cardBack, (screenW/2)-cardBack.getWidth()-10, (screenH/2)-(cardBack.getHeight()/2), null);
+
+        if (!discardPile.isEmpty()) {
+            canvas.drawBitmap(discardPile.get(0).getBitmap(), (screenW/2)+10, (screenH/2)-(cardBack.getHeight()/2), null);
+        }
+
+        for (int i = 0; i < myHand.size(); i++) {
+            if (i == movingCardIdx) {
+                canvas.drawBitmap(myHand.get(i).getBitmap(), movingX, movingY, null);
+            } else {
+                canvas.drawBitmap(myHand.get(i).getBitmap(), i*(scaledCardW+5), screenH-scaledCardH-whitePaint.getTextSize()-(50*scale), null);
+            }
+        }
+        invalidate();
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -55,12 +100,26 @@ public class GameView extends View {
         switch (eventaction ) {
 
             case MotionEvent.ACTION_DOWN:
+                if (myTurn) {
+                    for (int i = 0; i < 7; i++) {
+                        if (X > i*(scaledCardW+5) &&
+                                X < i*(scaledCardW+5)+scaledCardW &&
+                                Y > screenH-scaledCardH-whitePaint.getTextSize()-(50*scale)) {
+                            movingCardIdx = i;
+                            movingX = X-(int)(30*scale);
+                            movingY = Y-(int)(70*scale);
+                        }
+                    }
+                }
                 break;
 
             case MotionEvent.ACTION_MOVE:
+                movingX = X-(int)(30*scale);
+                movingY = Y-(int)(70*scale);
                 break;
 
             case MotionEvent.ACTION_UP:
+                movingCardIdx = -1;
                 break;
         }
         invalidate();
